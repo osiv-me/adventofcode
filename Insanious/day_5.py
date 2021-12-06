5383c1db0635dd2bad03d005a572c5d15fd3e0f6
Anton <anton.lindstrand0@gmail.com>
2021-12-06 18:02:10 +0100

    Minor improvement

--- Insanious/day_5.py
+++ Insanious/day_5.py
@@ -9,21 +9,21 @@ def part_one():
 	for line in data:
 		vec = line.split(' -> ')
 		v1, v2 = vec[0].split(','), vec[1].split(',')
-		v1 = [int(x) for x in v1]
-		v2 = [int(x) for x in v2]
 		if (not(v1[0] == v2[0] or v1[1] == v2[1])): # not horizontal or vertical
 			continue
+		v1 = [int(x) for x in v1]
+		v2 = [int(x) for x in v2]
 
 		if v1[0] == v2[0]: # vertical line
 			len = v2[1] - v1[1]
-			for i in range(abs(len) + 1): # magic number(?)
-				if len < 1:		grid[v1[0]][v1[1] - i] += 1
-				else:			grid[v1[0]][v1[1] + i] += 1
+			mult = -1 if len < 1 else 1
+			for i in range(abs(len) + 1):
+				grid[v1[0]][v1[1] + i*mult] += 1
 		else: # horizontal line
 			len = v2[0] - v1[0]
-			for i in range(abs(len) + 1): # magic number(?)
-				if len < 1:		grid[v1[0] - i][v1[1]] += 1
-				else:			grid[v1[0] + i][v1[1]] += 1
+			mult = -1 if len < 1 else 1
+			for i in range(abs(len) + 1):
+				grid[v1[0] + i*mult][v1[1]] += 1
 
 	# grid = map(list, zip(*grid)) # transpose for visuals
 	return sum([1 for row in grid for col in row if col > 1])
