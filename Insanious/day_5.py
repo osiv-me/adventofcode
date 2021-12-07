0c6c4e3d9b2083590ef8815dfe73d94ffb74c2e8
Anton <anton.lindstrand0@gmail.com>
2021-12-07 07:38:50 +0100

    finished day 5 part two

--- Insanious/day_5.py
+++ Insanious/day_5.py
@@ -29,7 +29,39 @@ def part_one():
 	return sum([1 for row in grid for col in row if col > 1])
 
 def part_two():
-	data = open('day_5.txt', 'r').read()
+	data = open('day_5.txt', 'r').read().split('\n')[:-1]
+	grid = [ [0]*1000 for i in range(1000) ]
+
+	for line in data:
+		vec = line.split(' -> ')
+		v1, v2 = vec[0].split(','), vec[1].split(',')
+		v1 = [int(x) for x in v1]
+		v2 = [int(x) for x in v2]
+		# skip non horizontal, vertical or diagonal lines
+		if not(v1[0] == v2[0] or v1[1] == v2[1] or (v1[0] == v2[1] and v1[1] == v2[0]) or (v1[0] == v1[1] and v2[1] == v2[0]) or (v2[0]-v1[1] == v2[0]-v1[1])):
+			continue
+
+		if v1[0] == v2[0]: # vertical line
+			length = v2[1] - v1[1]
+			mult = -1 if length < 1 else 1
+			for i in range(abs(length) + 1):
+				grid[v1[0]][v1[1] + i*mult] += 1
+		elif v1[1] == v2[1]: # horizontal line
+			length = v2[0] - v1[0]
+			mult = -1 if length < 1 else 1
+			for i in range(abs(length) + 1):
+				grid[v1[0] + i*mult][v1[1]] += 1
+		else: # diagonal line
+			xlen = v2[0] - v1[0]
+			ylen = v2[1] - v1[1]
+			xmult = -1 if xlen < 1 else 1
+			ymult = -1 if ylen < 1 else 1
+			# print(v1, v2, xlen, ylen, xmult, ymult)
+			for i in range(abs(xlen) + 1):
+				# print(v1[0] + i*xmult, v1[1] + i*ymult)
+				grid[v1[0] + i*xmult][v1[1] + i*ymult] += 1
+
+	return sum([1 for row in grid for col in row if col > 1])
 
 
 if __name__ == '__main__':
